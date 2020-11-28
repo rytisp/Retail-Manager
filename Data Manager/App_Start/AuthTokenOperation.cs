7f5187287c1df105970ba2c07cedba7207349f7f
rytis <rytisp@gmail.com>
2020-11-28 17:55:12 +0000

    Swagger Auth Token is working

--- Data Manager/App_Start/AuthTokenOperation.cs
+++ Data Manager/App_Start/AuthTokenOperation.cs
@@ -27,7 +27,8 @@ namespace Data_Manager.App_Start
                             type = "string",
                             name = "grant_type",
                             required = true,
-                            @in = "formData"
+                            @in = "formData",
+                            @default = "password"
                         },
                        new Parameter
                         {
